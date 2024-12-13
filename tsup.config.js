83b87db0a825558e6dca4b16d0c393c4cfadf781
irfan798 <irfan@contentwise.info>
2024-12-13 14:56:30 +0300

    feat: build commonjs modules as well

--- tsup.config.js
+++ tsup.config.js
@@ -1,3 +1,4 @@
+import {cjsImportsPlugin} from './tools/imports-plugin.tsup.js';
 import {defineConfig} from 'tsup';
 import {fileURLToPath} from 'node:url';
 import fs from 'node:fs/promises';
@@ -6,20 +7,39 @@ import path from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Get all TypeScript files in the src directory as entry points
 const files = await fs.readdir(path.join(__dirname, 'src'));
 const entry = files
   .filter(f => f.endsWith('.ts'))
   .map(f => path.join('src', f));
 
-export default defineConfig({
+const commonSettings = {
   clean: true,
-  dts: true,
+  dts: {
+    resolve: true, // Ensures all dependencies are included
+    entry, // Generate `.d.ts` for each entry file
+  },
   entry,
-  format: ['esm', 'cjs'],
-  minify: true,
-  outDir: 'lib',
-  sourcemap: false,
+  minify: false,
+  sourcemap: true,
   splitting: false,
   target: 'es2022',
   bundle: false,
-});
+  cjsInterop: true,
+};
+
+export default defineConfig([
+  // ESM configuration
+  {
+    ...commonSettings,
+    format: ['esm'],
+    outDir: 'lib',
+  },
+  // CJS configuration
+  {
+    ...commonSettings,
+    format: ['cjs'],
+    outDir: 'lib/cjs',
+    plugins: [cjsImportsPlugin()],
+  },
+]);
