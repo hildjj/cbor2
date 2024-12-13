d5fded94b79719bc9e5ad05c7b54b295cc10355d
irfan798 <irfan@contentwise.info>
2024-12-13 17:38:30 +0300

    fix: use regex for entry instead of fs

--- tsup.config.js
+++ tsup.config.js
@@ -1,26 +1,11 @@
 import {cjsImportsPlugin} from './tools/imports-plugin.tsup.js';
 import {defineConfig} from 'tsup';
-import {fileURLToPath} from 'node:url';
-import fs from 'node:fs/promises';
-import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Get all TypeScript files in the src directory as entry points
-const files = await fs.readdir(path.join(__dirname, 'src'));
-const entry = files
-  .filter(f => f.endsWith('.ts'))
-  .map(f => path.join('src', f));
 
 const commonSettings = {
   clean: true,
-  dts: {
-    resolve: true, // Ensures all dependencies are included
-    entry, // Generate `.d.ts` for each entry file
-  },
-  entry,
-  minify: false,
+  dts: true,
+  entry: ['src/*.ts'],
+  minify: true,
   sourcemap: true,
   splitting: false,
   target: 'es2022',
